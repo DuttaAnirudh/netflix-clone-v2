5c4b70c9587121631fc9476bf69b1132a92843ed
DuttaAnirudh <worktodutta@gmail.com>
2024-04-02 17:05:28 +0530

    chore: add base URL and KEY for fetch request from tmdB

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3/movie";
+export const API_URL_GENRE = "https://api.themoviedb.org/3/genre/movie";
+export const API_URL_TRENDING = "https://api.themoviedb.org/3/trending/movie";
+export const API_URL_SEARCH = "https://api.themoviedb.org/3/search/movie";
+export const KEY = "2f6c6847842dc1de1c6cba9837b4256f";
+export const API_URL_BASE_IMAGE = "https://image.tmdb.org/t/p/original";
