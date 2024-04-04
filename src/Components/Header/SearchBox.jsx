53183bc10e1b8d52472a6033e847d7c87bc58a5e
DuttaAnirudh <worktodutta@gmail.com>
2024-04-04 16:44:52 +0530

    feat: add focus on input element on search icon click function to search bar

--- src/Components/Header/SearchBox.jsx
+++ src/Components/Header/SearchBox.jsx
@@ -1,6 +1,13 @@
 import SearchIcon from "../../assets/search.png";
+import { useRef } from "react";
 
 const SearchBox = () => {
+  const formRef = useRef(null);
+
+  const handleInputFocusOnClick = () => {
+    formRef.current.focus();
+  };
+
   return (
     <div className="search__box">
       <form className="search__wrapper" id="search-form">
@@ -10,9 +17,15 @@ const SearchBox = () => {
           name="search"
           placeholder="Search any movies..."
           id="search-box"
+          ref={formRef}
         />
 
-        <img src={SearchIcon} alt="search" className="search__icon" />
+        <img
+          src={SearchIcon}
+          alt="search"
+          className="search__icon"
+          onClick={handleInputFocusOnClick}
+        />
       </form>
     </div>
   );
